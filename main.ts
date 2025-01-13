0f32323a809f8198175d3aadd33f8b49963527fe
Bubu <carr0t-bu@outlook.com>
2025-01-13 17:00:30 +0800

    first commit

---
+++ main.ts
@@ -0,0 +1,45 @@
+import { Hono } from 'hono'
+import { getConnInfo } from 'hono/cloudflare-workers'
+
+const curlUserAgent = 'curl/7.81.0'
+const ipInfoUrl = 'https://ipinfo.io'
+
+const app = new Hono()
+
+
+app.get('/', async (c) => {
+  const info = await getConnInfo(c)
+  const ip = info.remote.address
+  if (!ip) {
+    return c.text('No IP address found', 404)
+  }
+  const ipInfo = await fetch(`${ipInfoUrl}/${ip}`, {
+    headers: {
+      'User-Agent': curlUserAgent,
+    },
+  })
+  const desc = await ipInfo.text()
+  const rss = `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+<title>IP Info: ${ip}</title>
+<channel>
+  <item>
+    <title>${ip}</title>
+    <description>
+    <pre>
+    <code>  
+${desc}
+    </code>
+    </pre>
+    </description>
+  </item>
+</channel>
+</rss>`
+  return c.text(rss, {
+    headers: {
+      'Content-Type': 'application/xml+rss'
+    }
+  })
+})
+
+Deno.serve(app.fetch)
